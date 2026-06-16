7df2205a7034c9c768ec51751e05d777f4cad8cf
Máté Varga <mateistvanvarga@gmail.com>
2026-06-16 15:06:33 +0200

    fix: improve formatting of Tyre Strategy section in RaceWeekend component

--- src/pages/RaceWeekend.tsx
+++ src/pages/RaceWeekend.tsx
@@ -1139,8 +1139,12 @@ export default function RaceWeekend() {
                 )}
 
                 {(trackerTab ?? "timing") === "strategy" && (
-                  <div className={`${PANEL} flex-1 flex flex-col overflow-hidden border-0`}>
-                    <div className={`${PANEL_TITLE} shrink-0`}>Tyre Strategy</div>
+                  <div
+                    className={`${PANEL} flex-1 flex flex-col overflow-hidden border-0`}
+                  >
+                    <div className={`${PANEL_TITLE} shrink-0`}>
+                      Tyre Strategy
+                    </div>
                     <div className="flex-1 min-h-0 overflow-auto">
                       <StrategyBar
                         stints={stints.data ?? []}
