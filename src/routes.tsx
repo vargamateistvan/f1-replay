71856c215cf00bbc114aa04acf1d8776190d5b9a
Máté Varga <mateistvanvarga@gmail.com>
2026-09-08 10:41:17 +0200

    feat: enhance ReleaseVersionLabel to include detailed timestamp formatting

--- src/routes.tsx
+++ src/routes.tsx
@@ -150,6 +150,11 @@ function ReleaseVersionLabel() {
             month: "short",
             day: "numeric",
             year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+            timeZone: "UTC",
           }).format(new Date(publishedAt)),
         );
       })
